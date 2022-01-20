5d54afb4ca4d64a4c610e5c41762a3f5d202386f
Onur Gündoğdu <gundogdusoftware@outlook.com>
2022-01-20 16:50:34 +0300

    Service Class

--- SpringReactTest/src/main/java/com/onurgundogdu/springreacttest/user/UserController.java
+++ SpringReactTest/src/main/java/com/onurgundogdu/springreacttest/user/UserController.java
@@ -1,9 +1,7 @@
 package com.onurgundogdu.springreacttest.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +11,6 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
-
-    private static final Logger log= LoggerFactory.getLogger(UserController.class);
-
 
     @PostMapping("/api/v1/users")
     public void createUser(@RequestBody User user)
---
+++ SpringReactTest/src/main/java/com/onurgundogdu/springreacttest/user/UserService.java
@@ -0,0 +1,4 @@
+package com.onurgundogdu.springreacttest.user;
+
+public class UserService {
+}
